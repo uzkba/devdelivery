5eaa86c487cd58478928b6cb895aed4cf6e7e07d
luizryke <luizhenr.contact@gmail.com>
2026-08-26 22:36:33 -0300

    fix(cors): corrige allow_origins e allow_methods/headers mal configurados

--- backend/main.py
+++ backend/main.py
@@ -20,10 +20,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173/"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
-    allow_methods=[""],  
-    allow_headers=[""], 
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(autenticacao_route.router)
